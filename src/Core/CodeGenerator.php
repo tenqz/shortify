6aef7681670797f4a02adcd374f98776e44c044c
Oleg Patsay <smmartbiz@gmail.com>
2025-04-17 15:55:18 +0300

    Added CodeGenerator class to generate short URLs using random bytes and MD5 hashing.

---
+++ src/Core/CodeGenerator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenqz\Shortify\Core;
+use Random\Randomizer;
+
+/**
+ * Code generator for short URLs.
+ */
+final class CodeGenerator
+{
+    /**
+     * The alphabet to use for the code.
+     */
+    private string $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+    /**
+     * Default code length.
+     */
+    private const DEFAULT_LENGTH = 6;
+
+    /**
+     * Generate a code for a given URL.
+     *
+     * @param string $url The URL to generate a code for
+     * @param int $length The length of the code to generate
+     * @return string The generated code
+     */
+    public function generate(string $url, int $length = self::DEFAULT_LENGTH): string
+    {
+        $randomizer = new Randomizer();
+        $bytes = $randomizer->getBytesFromString($this->alphabet, $length);
+ 
+        return substr(md5($url . microtime(true) . $bytes), 0, $length);
+    }
+} 
